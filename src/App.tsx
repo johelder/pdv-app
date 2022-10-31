f3175c0c0ab5bce7cf71910ac29377fda756f911
johelder <johelder.araujo@pede.ai>
2022-10-30 22:48:28 -0300

    feat: create app routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import { NavigationButton } from './components/NavigationButton';
-
 import { ThemeProvider } from 'styled-components';
+import { Routes } from './routes';
 import { theme } from './styles/theme';
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <NavigationButton />
+      <Routes />
     </ThemeProvider>
   );
 };
---
+++ src/routes/app.stack.routes.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { Home } from '../pages';
+
+const Stack = createNativeStackNavigator();
+
+export const AppStackRoutes = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Home" component={Home} />
+    </Stack.Navigator>
+  );
+};
