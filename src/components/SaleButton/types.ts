ea725368674444aa90b681295bc844c151410744
johelder <johelder.araujo@pede.ai>
2022-11-02 15:01:21 -0300

    refactor: using sale button types in a external file

---
+++ src/components/SaleButton/types.ts
@@ -0,0 +1,6 @@
+import { TouchableOpacityProps } from 'react-native';
+import { ISale } from '../../pages/Home/types';
+
+export interface ISaleButtonProps extends TouchableOpacityProps {
+  sale: ISale;
+}
