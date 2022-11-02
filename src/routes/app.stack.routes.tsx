8ef4cd49be1a862a6a61ea0268abff8eeb821561
johelder <johelder.araujo@pede.ai>
2022-11-02 20:05:18 -0300

    feat: create new sale page at routes

--- src/routes/app.stack.routes.tsx
+++ src/routes/app.stack.routes.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { RootStackParamList } from './types';
 
-import { Home } from '../pages';
+import { Home, NewSale } from '../pages';
+import { useTheme } from 'styled-components';
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export const AppStackRoutes = () => {
+  const theme = useTheme();
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
+    <Stack.Navigator>
+      <Stack.Screen
+        name="Home"
+        component={Home}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="NewSale"
+        component={NewSale}
+        options={{
+          title: 'Nova venda',
+          headerTitleStyle: {
+            fontFamily: theme.fonts.primary.bold,
+          },
+          headerStyle: {
+            backgroundColor: theme.colors.background,
+          },
+          headerTintColor: theme.colors.primary,
+        }}
+      />
     </Stack.Navigator>
   );
 };
