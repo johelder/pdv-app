962ddd4b9bd03f46e88f5c872f3cf18eb43f251b
johelder <johelder.araujo@pede.ai>
2022-11-02 15:02:18 -0300

    feat: create home types

---
+++ src/pages/Home/types.ts
@@ -0,0 +1,32 @@
+export interface IProduct {
+  id: number;
+  name: string;
+  code: string;
+  description: string;
+  status: string;
+  image: string;
+  price: number;
+}
+
+export interface IProductSold
+  extends Pick<IProduct, 'name' | 'code' | 'image'> {
+  id: number;
+  unity_price: number;
+  quantity: number;
+  sale_id: number;
+}
+
+export interface IPaymentMethod {
+  id: number;
+  name: string;
+}
+
+export interface ISale {
+  id: number;
+  daily_position: 1 | null;
+  change: number;
+  total: number;
+  createdAt: string;
+  products_sold: IProductSold[];
+  payment_method: IPaymentMethod;
+}
