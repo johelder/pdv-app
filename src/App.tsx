e199bbfaaac9cb49917c6d9d912ec40b1904d20e
johelder <johelder.araujo@pede.ai>
2022-11-05 18:17:11 -0300

    chore: install and configure react-native-reanimated

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { ThemeProvider } from 'styled-components';
 import { Routes } from './routes';
@@ -6,8 +7,10 @@ import { theme } from './styles/theme';
 
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <Routes />
-    </ThemeProvider>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </GestureHandlerRootView>
   );
 };
