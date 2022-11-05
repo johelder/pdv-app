135ae67547a2bf1d070acc9129d93daef27ba661
johelder <johelder.araujo@pede.ai>
2022-11-05 12:50:56 -0300

    feat: create select products page

---
+++ src/pages/SelectProducts/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export const SelectProducts = () => {
+  return (
+    <View>
+      <Text>Select Product Page</Text>
+    </View>
+  );
+};
