53537010c93d0d460f61fcf0242b52ffb62a0b33
johelder <johelder.araujo@pede.ai>
2022-11-06 12:41:42 -0300

    feat: create categories buttons

--- src/pages/SelectProducts/index.tsx
+++ src/pages/SelectProducts/index.tsx
@@ -1,19 +1,82 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 
-import { DraggableButton } from '../../components';
+import { Button, DraggableButton, TextInput } from '../../components';
+import { categories } from './data';
 
 import { useTheme } from 'styled-components';
 
 import * as S from './styles';
+import { ListRenderItemInfo } from 'react-native';
+import { ICategory } from './types';
 
 export const SelectProducts = () => {
+  const [activeCategory, setActiveCategory] = useState(1);
   const theme = useTheme();
+
+  const handleActiveCategory = useCallback((categoryId: number) => {
+    setActiveCategory(categoryId);
+  }, []);
+
+  const renderCategory = useCallback(
+    ({ item: category }: ListRenderItemInfo<ICategory>) => {
+      return (
+        <S.CategoryContainer>
+          <Button.Root
+            type={activeCategory === category.id ? 'filled' : 'outline'}
+            color={theme.colors.categories}
+            onPress={() => handleActiveCategory(category.id)}
+          >
+            <Button.Text
+              color={
+                activeCategory === category.id
+                  ? theme.colors.light
+                  : theme.colors.dark
+              }
+            >
+              {category.name}
+            </Button.Text>
+          </Button.Root>
+        </S.CategoryContainer>
+      );
+    },
+    [
+      activeCategory,
+      handleActiveCategory,
+      theme.colors.categories,
+      theme.colors.dark,
+      theme.colors.light,
+    ],
+  );
 
   return (
     <S.Container>
       <DraggableButton color={theme.colors.products}>
         <S.BagIcon name="shopping-bag" />
       </DraggableButton>
+
+      <S.SearchInputContainer>
+        <TextInput.Root>
+          <TextInput.Icon>
+            <S.SearchIcon name="search" />
+          </TextInput.Icon>
+
+          <TextInput.Input
+            placeholder="Pesquisar um produto"
+            placeholderTextColor={theme.colors.dark}
+            autoCorrect={false}
+            autoComplete="off"
+            autoCapitalize="none"
+          />
+        </TextInput.Root>
+      </S.SearchInputContainer>
+
+      <S.CategoriesContainer>
+        <S.Categories
+          data={categories}
+          renderItem={renderCategory}
+          keyExtractor={category => String(category.id)}
+        />
+      </S.CategoriesContainer>
     </S.Container>
   );
 };
--- src/pages/SelectProducts/styles.ts
+++ src/pages/SelectProducts/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components/native';
+import { FlatList, FlatListProps } from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
+import { ICategory } from './types';
 
 export const Container = styled.SafeAreaView`
   flex: 1;
@@ -9,4 +11,34 @@ export const Container = styled.SafeAreaView`
 export const BagIcon = styled(Feather)`
   font-size: 28px;
   color: ${({ theme }) => theme.colors.light};
+`;
+
+export const SearchInputContainer = styled.View`
+  margin: 20px 15px 0;
+`;
+
+export const SearchIcon = styled(Feather)`
+  font-size: 24px;
+  color: ${({ theme }) => theme.colors.dark};
+`;
+
+export const CategoriesContainer = styled.View`
+  margin: 20px 0;
+`;
+
+export const Categories = styled(
+  FlatList as new (props: FlatListProps<ICategory>) => FlatList<ICategory>,
+).attrs({
+  showsHorizontalScrollIndicator: false,
+  horizontal: true,
+  contentContainerStyle: {
+    paddingRight: 15,
+  },
+})`
+  padding-left: 15px;
+`;
+
+export const CategoryContainer = styled.View`
+  width: 250px;
+  margin-right: 15px;
 `;
