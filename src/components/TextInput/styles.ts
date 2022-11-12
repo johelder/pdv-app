a67dd219c3e273a6c73c207b5645f30c23f9abfe
johelder <johelder.araujo@pede.ai>
2022-11-12 13:13:14 -0300

    refactor: adjust text input padding

--- src/components/TextInput/styles.ts
+++ src/components/TextInput/styles.ts
@@ -19,7 +19,7 @@ export const TextInputRootContent = styled.View<ITextInputRootProps>`
     isDisabled ? theme.colors.gray_100 : theme.colors.light};
   border-radius: 25px;
 
-  padding: 0 10px;
+  padding: 0 15px;
 
   flex-direction: row;
   align-items: center;
