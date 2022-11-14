a6a3fadcbda70c9403db3bb59d6fa236cb1c430a
johelder <johelder.araujo@pede.ai>
2022-11-14 12:45:07 -0300

    feat: adding select categories page to index pages

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,5 +2,6 @@ import { Home } from './Home';
 import { NewSale } from './NewSale';
 import { SelectProducts } from './SelectProducts';
 import { RegisterProduct } from './RegisterProduct';
+import { SelectCategories } from './SelectCategories';
 
-export { Home, NewSale, SelectProducts, RegisterProduct };
+export { Home, NewSale, SelectProducts, RegisterProduct, SelectCategories };
