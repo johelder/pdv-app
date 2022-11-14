36fd4c7078382125734704a3eb8880b625627252
johelder <johelder.araujo@pede.ai>
2022-11-14 18:03:59 -0300

    feat: create app provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
+import { AppProvider } from './contexts';
 
 import { store } from './store';
 import { Routes } from './routes';
@@ -11,11 +12,13 @@ import { theme } from './styles/theme';
 export const App = () => {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Provider store={store}>
-        <ThemeProvider theme={theme}>
-          <Routes />
-        </ThemeProvider>
-      </Provider>
+      <AppProvider>
+        <Provider store={store}>
+          <ThemeProvider theme={theme}>
+            <Routes />
+          </ThemeProvider>
+        </Provider>
+      </AppProvider>
     </GestureHandlerRootView>
   );
 };
