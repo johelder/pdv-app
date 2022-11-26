052a4bb3f9600d59bcbabdec05c8d7b408825075
luis-szn <luisfelipe.szn@gmail.com>
2022-11-26 11:33:01 -0300

    style: adjust navigation buttons positions

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -66,7 +66,7 @@ export const NavigationButtonsContainer = styled.ScrollView.attrs({
   },
 })`
   position: absolute;
-  bottom: -58px;
+  top: 270px;
 `;
 
 export const DailySalesContainer = styled.View`
