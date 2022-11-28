52763af9a4c59640f0e0a35c9556d94f3d045c51
johelder <johelder.araujo@pede.ai>
2022-11-28 19:35:00 -0300

    fix: adding redirect to registered categories page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -29,7 +29,9 @@ export const Home = ({ navigation }: THomeProps) => {
   };
 
   const handleRedirectToRegisterProduct = () => {
-    navigation.navigate('RegisterProduct');
+    navigation.navigate('RegisterProduct', {
+      redirect: 'RegisteredCategories',
+    });
   };
 
   const handleRedirectToRegisterCategory = () => {
